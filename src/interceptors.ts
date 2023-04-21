98a19a2101f2c8c57b15cef43442ceb99e8317da
Andrew Smiley <andrews@uriux.com>
2023-04-21 09:48:36 -0400

    fix: restore typebox dto typecheck in interceptor

--- src/interceptors.ts
+++ src/interceptors.ts
@@ -1,9 +1,9 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { DECORATORS } from '@nestjs/swagger/dist/constants';
-import { TypeboxDto } from './create-dto';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { isTypeboxDto } from './util';
 
 @Injectable()
 export class TypeboxTransformInterceptor implements NestInterceptor {
@@ -13,9 +13,9 @@ export class TypeboxTransformInterceptor implements NestInterceptor {
         return next.handle().pipe(
             map(data => {
                 const responseMeta = this.reflector.get(DECORATORS.API_RESPONSE, context.getHandler());
-                const dto = (responseMeta['200'] || responseMeta['201'] || {})['type'] as TypeboxDto | undefined;
+                const dto = (responseMeta['200'] || responseMeta['201'] || {})['type'];
 
-                if (!dto) return data;
+                if (!isTypeboxDto(dto)) return data;
 
                 if (Array.isArray(data)) {
                     return data.map(datum => dto.validate(datum));
